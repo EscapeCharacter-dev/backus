f2efe31629ddbeefc29c5e1c68596bdc11f98906
EscapeCharacter-dev <30359139+EscapeCharacter-dev@users.noreply.github.com>
2021-10-01 22:15:28 -0400

    More operators

--- ksc/GenModules/Gen-x86_64.c
+++ ksc/GenModules/Gen-x86_64.c
@@ -2,12 +2,14 @@
 #include "../NodeKinds.h"
 #include <stdio.h>
 
+static int64_t symbols = 0;
+
 static int64_t genExpression(KscTree *tree)
 {
 	if (!tree) return 0;
 
-	if (tree->right) genExpression(tree->right);
 	if (tree->left) genExpression(tree->left);
+	if (tree->right) genExpression(tree->right);
 
 	switch (tree->kind)
 	{
@@ -90,6 +92,125 @@ static int64_t genExpression(KscTree *tree)
 		"\tpop rax\n"
 		"\tsub rax, rbx\n"
 		"\tpush rax\n");
+		return 0;
+	case KSC_TREE_LSHIFT:
+		fprintf(stdout,
+		"\tpop rcx\n"
+		"\tpop rax\n"
+		"\tsal rax, cl\n"
+		"\tpush rax\n");
+		return 0;
+	case KSC_TREE_RSHIFT:
+		fprintf(stdout,
+		"\tpop rcx\n"
+		"\tpop rax\n"
+		"\tsar rax, cl\n"
+		"\tpush rax\n");
+		return 0;
+	case KSC_TREE_LOWER:
+		fprintf(stdout,
+		"\tpop rbx\n"
+		"\tpop rax\n"
+		"\tcmp rax, rbx\n"
+		"\tsetl al\n"
+		"\tmovzx rax, al\n");
+		return 0;
+	case KSC_TREE_LOWER_EQUAL:
+		fprintf(stdout,
+		"\tpop rbx\n"
+		"\tpop rax\n"
+		"\tcmp rax, rbx\n"
+		"\tsetle al\n"
+		"\tmovzx rax, al\n");
+		return 0;
+	case KSC_TREE_GREATER:
+		fprintf(stdout,
+		"\tpop rbx\n"
+		"\tpop rax\n"
+		"\tcmp rax, rbx\n"
+		"\tsetg al\n"
+		"\tmovzx rax, al\n");
+		return 0;
+	case KSC_TREE_GREATER_EQUAL:
+		fprintf(stdout,
+		"\tpop rbx\n"
+		"\tpop rax\n"
+		"\tcmp rax, rbx\n"
+		"\tsetge al\n"
+		"\tmovzx rax, al\n");
+		return 0;
+	case KSC_TREE_EQUAL:
+		fprintf(stdout,
+		"\tpop rbx\n"
+		"\tpop rax\n"
+		"\tcmp rax, rbx\n"
+		"\tsete al\n"
+		"\tmovzx rax, al\n");
+		return 0;
+	case KSC_TREE_NOT_EQUAL:
+		fprintf(stdout,
+		"\tpop rbx\n"
+		"\tpop rax\n"
+		"\tcmp rax, rbx\n"
+		"\tsetne al\n"
+		"\tmovzx rax, al\n");
+		return 0;
+	case KSC_TREE_BITWISE_AND:
+		fprintf(stdout,
+		"\tpop rbx\n"
+		"\tpop rax\n"
+		"\tand rax, rbx\n"
+		"\tpush rax\n");
+		return 0;
+	case KSC_TREE_BITWISE_XOR:
+		fprintf(stdout,
+		"\tpop rbx\n"
+		"\tpop rax\n"
+		"\txor rax, rbx\n"
+		"\tpush rax\n");
+		return 0;
+	case KSC_TREE_BITWISE_OR:
+		fprintf(stdout,
+		"\tpop rbx\n"
+		"\tpop rax\n"
+		"\tor rax, rbx\n"
+		"\tpush rax\n");
+		return 0;
+	case KSC_TREE_LOGICAL_AND:
+		{
+			const int64_t s0 = symbols++;
+			const int64_t s1 = symbols++;
+			fprintf(stdout,
+			"\tpop rbx\n"
+			"\tpop rax\n"
+			"\ttest rax, rax\n"
+			"\tjmp .L%ld\n"
+			"\ttest rbx, rbx\n"
+			"\tjmp .L%ld\n"
+			"\tmov rax, 1\n"
+			"\tjmp .L%ld\n"
+			".L%ld: mov rax, 0\n"
+			".L%ld: push rax\n", s0, s0, s1, s0, s1);
+		}
+		return 0;
+	case KSC_TREE_LOGICAL_OR:
+		{
+			const int64_t s0 = symbols++;
+			const int64_t s1 = symbols++;
+			const int64_t s2 = symbols++;
+			fprintf(stdout,
+			"\tpop rbx\n"
+			"\tpop rax\n"
+			"\ttest rax, rax\n"
+			"\tjmp .L%ld\n"
+			"\ttest rbx, rbx\n"
+			"\tjmp .L%ld\n"
+			".L%ld:\n"
+			"\tmov rax, 1\n"
+			"\tjmp .L%ld\n"
+			".L%ld: mov rax, 0\n"
+			".L%ld: push rax\n", s0, s1, s1, s2, s1, s2);
+		}
 		return 0;
 	}
 }
